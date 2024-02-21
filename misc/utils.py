b11f8b80651e4a0c26be76abba810a77dac9ecdd
Patrick Wang <wang.patrick57@gmail.com>
2024-02-21 10:43:36 -0500

    done with conv_inputpath_to_abspath (tested in cli)

--- misc/utils.py
+++ misc/utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 def conv_inputpath_to_abspath(inputpath: str) -> str:
     '''
@@ -7,23 +8,34 @@ def conv_inputpath_to_abspath(inputpath: str) -> str:
     Whenever a path is required, the user is allowed to enter relative paths, absolute paths, or paths starting with ~
     We assume that the user only ever runs main.py so they will always be in the base repo dir (dbgym/). Thus, all
         relative paths are relative to that
-    Raises FileNotFoundError if the path is malformed or doesn't exist
-    Raises RuntimeError for other errors
+    It *does not* check whether the path exists, since the user might be wanting to create a new file/dir
+    Raises RuntimeError for errors
     '''
+    # (most) checks
+    # regardless of whether the user wants an absolute, relative, or home path, I will do all checks
+    # this helps errors surface more quickly
     assert type(inputpath) is str
     if len(inputpath) == 0:
-        raise FileNotFoundError(f'inputpath ({inputpath}) is empty')
+        raise RuntimeError(f'inputpath ({inputpath}) is empty')
     cwd = os.getcwd()
     if not is_in_base_git_dir(cwd):
         raise RuntimeError(f'cwd ({cwd}) is not the base directory of a git repo. Please run main.py from the base dbgym/ directory')
+
+    # logic
+    if inputpath[0] == '~':
+        return os.path.normpath(os.path.expanduser(inputpath))
+    elif inputpath[0] == '/':
+        return os.path.normpath(inputpath)
+    else:
+        return os.path.normpath(os.path.join(cwd, inputpath))
 
 def is_in_base_git_dir(cwd) -> bool:
     '''
     Returns whether we are in the base directory of some git repository
     '''
     try:
-        git_top_level = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], encoding='utf-8').strip()
-        return git_top_level == cwd
+        git_toplevel = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], encoding='utf-8').strip()
+        return git_toplevel == cwd
     except subprocess.CalledProcessError as e:
         # this means we are not in _any_ git repo
         return False
