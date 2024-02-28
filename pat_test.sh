53d95957bd1d6261d191ec97db976111a6b3bebf
Patrick Wang <wang.patrick57@gmail.com>
2024-02-28 17:38:13 +0000

    fixed AttributeError

--- pat_test.sh
+++ pat_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python task.py --no-startup-check protox embedding datagen tpch --connection-str "host=localhost port=5432 dbname=benchbase user=admin"
-# python task.py --no-startup-check protox embedding train tpch --iterations-per-epoch 1 --num-samples 4 --train-max-concurrent 4 --num-points-to-sample 32 --max-segments 3
+python task.py --no-startup-check protox embedding datagen tpch --connection-str "host=localhost port=5432 dbname=benchbase user=admin" --override-sample-limits "lineitem,32768"
+python task.py --no-startup-check protox embedding train tpch --iterations-per-epoch 1 --num-samples 4 --train-max-concurrent 4 --num-points-to-sample 32 --max-segments 3
