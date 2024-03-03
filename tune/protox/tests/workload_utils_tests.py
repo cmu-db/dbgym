0aa714d25ce900860b1a0943c5effddcbe67b2e4
Patrick Wang <wang.patrick57@gmail.com>
2024-03-03 23:58:15 +0000

    test_extract_aliases

--- tune/protox/tests/workload_utils_tests.py
+++ tune/protox/tests/workload_utils_tests.py
@@ -1,42 +1,48 @@
 import unittest
 import pglast
 
-from tune.protox.env.pglast3_workload_utils import QueryType, extract_sqltypes
+import tune.protox.env.pglast3_workload_utils as pl3_utils
 
 class WorkloadUtilsTests(unittest.TestCase):
-    TPCH_Q1 = '''
-select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
-from
-	lineitem
-where
-	l_shipdate <= date '1998-12-01' - interval '80' day
-group by
-	l_returnflag,
-	l_linestatus
-order by
-	l_returnflag,
-	l_linestatus;
-'''
+    @staticmethod
+    def pglast3_parse(sql):
+        return pglast.Node(pglast.parse_sql(sql))
+    
+
+    @staticmethod
+    def pglast_parse(sql):
+        # return pglast.parse_sql(sql)
+        return WorkloadUtilsTests.pglast3_parse(sql)
+
+
+    def test_extract_aliases(self):
+        sql = "select * from t1 as t1_alias; select * from t1;"
+        stmts = WorkloadUtilsTests.pglast_parse(sql)
+        aliases = pl3_utils.extract_aliases(stmts)
+        self.assertTrue('t1' in aliases and len(aliases) == 1)
+        self.assertEqual(set(aliases['t1']), set(['t1', 't1_alias']))
+
 
     def test_extract_sqltypes(self):
-        sql = WorkloadUtilsTests.TPCH_Q1
-        stmts = pglast.Node(pglast.parse_sql(sql))
+        sql = '''
+select * from t1;
+update t1 set t1.c1 = 0 where t1.c1 = 1;
+create or replace view view1 (view1_c1) as
+    select c1
+    from t1;
+'''
+        stmts = WorkloadUtilsTests.pglast_parse(sql)
         pid = 0
-        sqltypes = extract_sqltypes(stmts, pid)
-        self.assertEqual(len(sqltypes), 1)
-        self.assertTrue(type(sqltypes[0]) is tuple and len(sqltypes[0]) == 2)
-        self.assertEqual(sqltypes[0][0], QueryType.SELECT)
+        sqltypes = pl3_utils.extract_sqltypes(stmts, pid)
 
+        expected_num_stmts = 3
+        self.assertEqual(len(sqltypes), expected_num_stmts)
+        for i in range(expected_num_stmts):
+            self.assertTrue(type(sqltypes[i]) is tuple and len(sqltypes[i]) == 2)
+        self.assertEqual(sqltypes[0][0], pl3_utils.QueryType.SELECT)
+        self.assertEqual(sqltypes[1][0], pl3_utils.QueryType.INS_UPD_DEL)
+        self.assertEqual(sqltypes[2][0], pl3_utils.QueryType.CREATE_VIEW)
+    
 
 if __name__ == '__main__':
     unittest.main()
