391fa993528494a3e4f2d1835c7ce941627620bf
Patrick Wang <wang.patrick57@gmail.com>
2024-03-04 00:45:34 +0000

    switched back to the regular workload_utils

--- tune/protox/tests/workload_utils_tests.py
+++ tune/protox/tests/workload_utils_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import pglast
 
-import tune.protox.env.pglast3_workload_utils as pl3_utils
+from tune.protox.env.workload_utils import *
 
 class WorkloadUtilsTests(unittest.TestCase):
     TPCH_TABLES = ["part", "partsupp", "lineitem", "orders", "supplier", "customer", "nation", "region"]
@@ -57,14 +57,14 @@ order by
 
     @staticmethod
     def pglast_parse(sql):
-        # return pglast.parse_sql(sql)
-        return WorkloadUtilsTests.pglast3_parse(sql)
+        return pglast.parse_sql(sql)
+        # return WorkloadUtilsTests.pglast3_parse(sql)
 
 
     def test_extract_aliases(self):
         sql = "select * from t1 as t1_alias; select * from t1;"
         stmts = WorkloadUtilsTests.pglast_parse(sql)
-        aliases = pl3_utils.extract_aliases(stmts)
+        aliases = extract_aliases(stmts)
         self.assertTrue("t1" in aliases and len(aliases) == 1)
         self.assertEqual(set(aliases["t1"]), set(["t1", "t1_alias"]))
 
@@ -79,15 +79,15 @@ create or replace view view1 (view1_c1) as
 """
         stmts = WorkloadUtilsTests.pglast_parse(sql)
         pid = 0
-        sqltypes = pl3_utils.extract_sqltypes(stmts, pid)
+        sqltypes = extract_sqltypes(stmts, pid)
 
         expected_num_stmts = 3
         self.assertEqual(len(sqltypes), expected_num_stmts)
         for i in range(expected_num_stmts):
             self.assertTrue(type(sqltypes[i]) is tuple and len(sqltypes[i]) == 2)
-        self.assertEqual(sqltypes[0][0], pl3_utils.QueryType.SELECT)
-        self.assertEqual(sqltypes[1][0], pl3_utils.QueryType.INS_UPD_DEL)
-        self.assertEqual(sqltypes[2][0], pl3_utils.QueryType.CREATE_VIEW)
+        self.assertEqual(sqltypes[0][0], QueryType.SELECT)
+        self.assertEqual(sqltypes[1][0], QueryType.INS_UPD_DEL)
+        self.assertEqual(sqltypes[2][0], QueryType.CREATE_VIEW)
 
 
     def test_extract_columns(self):
@@ -95,10 +95,10 @@ create or replace view view1 (view1_c1) as
         tables = WorkloadUtilsTests.TPCH_TABLES
         all_attributes = WorkloadUtilsTests.TPCH_ALL_ATTRIBUTES
         stmts = WorkloadUtilsTests.pglast_parse(sql)
-        aliases = pl3_utils.extract_aliases(stmts)
+        aliases = extract_aliases(stmts)
         self.assertEqual(len(stmts), 1)
         stmt = stmts[0]
-        tbl_col_usages, all_refs = pl3_utils.extract_columns(stmt, tables, all_attributes, aliases)
+        tbl_col_usages, all_refs = extract_columns(stmt, tables, all_attributes, aliases)
         
         for table in tables:
             self.assertTrue(table in tbl_col_usages)
@@ -130,10 +130,10 @@ from cte1;
         tables = ["t1"]
         all_attributes = {"c1": "t1", "c2": "t1"}
         stmts = WorkloadUtilsTests.pglast_parse(sql)
-        aliases = pl3_utils.extract_aliases(stmts)
+        aliases = extract_aliases(stmts)
         self.assertEqual(len(stmts), 1)
         stmt = stmts[0]
-        tbl_col_usages, all_refs = pl3_utils.extract_columns(stmt, tables, all_attributes, aliases)
+        tbl_col_usages, all_refs = extract_columns(stmt, tables, all_attributes, aliases)
         
         self.assertEqual(tbl_col_usages, {"t1": {"c2"}})
         self.assertEqual(set(all_refs), set([("t1", "c1"), ("t1", "c2"), ("t1", "c1"), ("t1", "c2")]))
