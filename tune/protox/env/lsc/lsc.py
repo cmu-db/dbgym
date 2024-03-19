fc315aff9540238bc173ef1e599cc9c8232848fc
Patrick Wang <wang.patrick57@gmail.com>
2024-03-19 16:26:53 -0400

    add lsc/

---
+++ tune/protox/env/lsc/lsc.py
@@ -0,0 +1,121 @@
+from typing import Any, Optional, TypeVar, cast
+
+import numpy as np
+import torch
+
+from tune.protox.env.logger import Logger
+from tune.protox.env.types import ProtoAction
+
+T = TypeVar("T", torch.Tensor, np.typing.NDArray[np.float32])
+
+
+class LSC(object):
+    def __init__(
+        self,
+        horizon: int,
+        lsc_parameters: dict[str, Any],
+        vae_config: dict[str, Any],
+        logger: Optional[Logger],
+    ):
+        self.frozen = False
+        self.horizon = horizon
+        self.num_steps = 0
+        self.num_episodes = 0
+        self.vae_configuration = vae_config
+
+        lsc_splits = lsc_parameters["initial"].split(",")
+        lsc_increments = lsc_parameters["increment"].split(",")
+        lsc_max = lsc_parameters["max"].split(",")
+        if len(lsc_splits) == 1:
+            lsc_splits = [float(lsc_splits[0])] * horizon
+        else:
+            assert len(lsc_splits) == horizon
+            lsc_splits = [float(f) for f in lsc_splits]
+
+        if len(lsc_increments) == 1:
+            lsc_increments = [float(lsc_increments[0])] * horizon
+        else:
+            assert len(lsc_increments) == horizon
+            lsc_increments = [float(f) for f in lsc_increments]
+
+        if len(lsc_max) == 1:
+            lsc_max = [float(lsc_max[0])] * horizon
+        else:
+            assert len(lsc_max) == horizon
+            lsc_max = [float(f) for f in lsc_max]
+
+        self.shift_eps_freq = lsc_parameters["shift_eps_freq"]
+        self.lsc_shift = np.array(lsc_splits)
+        self.increment = np.array(lsc_increments)
+        self.max = np.array(lsc_max)
+        self.shift_after = lsc_parameters["shift_after"]
+        self.logger = logger
+
+        if self.logger:
+            self.logger.get_logger(__name__).info("LSC Shift: %s", self.lsc_shift)
+            self.logger.get_logger(__name__).info(
+                "LSC Shift Increment: %s", self.increment
+            )
+            self.logger.get_logger(__name__).info("LSC Shift Max: %s", self.max)
+
+    def apply_bias(self, action: ProtoAction) -> ProtoAction:
+        assert action.shape[-1] == self.vae_configuration["latent_dim"], print(action.shape, self.vae_configuration["latent_dim"])
+
+        # Get the LSC shift associated with the current episode.
+        lsc_shift = self.lsc_shift[(self.num_steps % self.horizon)]
+        lsc_shift = lsc_shift * self.vae_configuration["output_scale"]
+        return ProtoAction(action + lsc_shift)
+
+    def current_bias(self) -> float:
+        # Get the LSC shift associated with the current episode.
+        lsc_shift = self.lsc_shift[(self.num_steps % self.horizon)]
+        lsc_shift = lsc_shift * self.vae_configuration["output_scale"]
+        return cast(float, lsc_shift)
+
+    def current_scale(self) -> np.typing.NDArray[np.float32]:
+        lsc_shift = self.lsc_shift[(self.num_steps % self.horizon)]
+        lsc_max = self.max[(self.num_steps % self.horizon)]
+        rel = lsc_shift / lsc_max
+        return np.array([(rel * 2.0) - 1], dtype=np.float32)
+
+    def inverse_scale(self, value: torch.Tensor) -> torch.Tensor:
+        lsc_max = self.max[0]
+        lsc_shift = ((value + 1) / 2.0) * lsc_max
+        return cast(torch.Tensor, lsc_shift * self.vae_configuration["output_scale"])
+
+    def advance(self) -> None:
+        if self.frozen:
+            return
+
+        self.num_steps += 1
+
+    def freeze(self) -> None:
+        self.frozen = True
+
+    def unfreeze(self) -> None:
+        self.frozen = False
+
+    def reset(self) -> None:
+        if self.frozen:
+            return
+
+        # Advance the episode count.
+        self.num_episodes += 1
+        if (self.num_episodes <= self.shift_after) or (
+            (self.num_episodes - self.shift_after) % self.shift_eps_freq != 0
+        ):
+            # Reset the number of steps we've taken.
+            self.num_steps = 0
+        else:
+            # Get how many steps to make the update on.
+            bound = self.horizon
+            self.num_steps = 0
+
+            # Now try to perform the LSC shifts.
+            # Increment the current bias with the increment.
+            self.lsc_shift[:bound] += self.increment[:bound]
+            self.lsc_shift = self.lsc_shift % self.max
+            if self.logger:
+                self.logger.get_logger(__name__).info(
+                    "LSC Bias Update: %s", self.lsc_shift
+                )
---
+++ tune/protox/env/lsc/lsc_wrapper.py
@@ -0,0 +1,47 @@
+from typing import Any, Optional, Tuple
+
+import gymnasium as gym
+
+from tune.protox.env.logger import Logger
+from tune.protox.env.lsc.lsc import LSC
+from tune.protox.env.target_reset.target_reset_wrapper import TargetResetWrapper
+
+
+class LSCWrapper(gym.Wrapper[Any, Any, Any, Any]):
+    def __init__(self, lsc: LSC, env: gym.Env[Any, Any], logger: Optional[Logger]):
+        assert not isinstance(env, TargetResetWrapper)
+        super().__init__(env)
+        self.lsc = lsc
+        self.logger = logger
+
+    def reset(self, *args: Any, **kwargs: Any) -> Tuple[Any, dict[str, Any]]:
+        state, info = self.env.reset(*args, **kwargs)
+        self.lsc.reset()
+
+        state["lsc"] = self.lsc.current_scale()
+        if self.logger:
+            lsc = state["lsc"]
+            self.logger.get_logger(__name__).debug(f"Attaching LSC: {lsc}")
+
+        return state, info
+
+    def step(
+        self, *args: Any, **kwargs: Any
+    ) -> Tuple[Any, float, bool, bool, dict[str, Any]]:
+        state, reward, term, trunc, info = self.env.step(*args, **kwargs)
+
+        # Remember the LSC when we considered this action.
+        info["lsc"] = self.lsc.current_scale()
+        old_bias = self.lsc.current_bias()
+        old_lsc = info["lsc"]
+
+        # Store the new LSC.
+        self.lsc.advance()
+        state["lsc"] = self.lsc.current_scale()
+        new_bias = self.lsc.current_bias()
+
+        if self.logger:
+            lsc = state["lsc"]
+            self.logger.get_logger(__name__).debug(f"Shifting LSC: {old_lsc} ({old_bias}) -> {lsc} ({new_bias})")
+
+        return state, float(reward), term, trunc, info
