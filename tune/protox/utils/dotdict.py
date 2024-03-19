1b576ecf422f060a383d95bf53f1f33bbb84799f
Patrick Wang <wang.patrick57@gmail.com>
2024-03-19 16:39:34 -0400

    del utils.dotdict.py

--- tune/protox/utils/dotdict.py
+++
@@ -1,8 +0,0 @@
-class DotDict(dict):
-    def __getattr__(self, name):
-        if name in self:
-            return self[name]
-        raise AttributeError(name)
-
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
