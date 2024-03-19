4cb6a2dd0c7b7dde871f2d3f7c27c653912283fb
Patrick Wang <wang.patrick57@gmail.com>
2024-03-19 16:23:46 -0400

    mod embedding/utils.py

--- tune/protox/embedding/utils.py
+++ tune/protox/embedding/utils.py
@@ -1,21 +1,8 @@
-import gc
-import logging
-import os
-
-import pandas as pd
-import torch
-from hyperopt import hp
-from sklearn.model_selection import train_test_split
-from torch.utils.data import TensorDataset
-
-from misc.utils import open_and_save
-from tune.protox.embedding.loss import COST_COLUMNS
-from tune.protox.env.space.index_policy import IndexRepr
-from tune.protox.env.space.index_space import IndexSpace
-from tune.protox.env.workload import Workload
+from typing import Any
+from hyperopt import hp # type: ignore
 
 
-def f_unpack_dict(dct):
+def f_unpack_dict(dct: dict[str, Any]) -> dict[str, Any]:
     """
     Unpacks all sub-dictionaries in given dictionary recursively.
     There should be no duplicated keys across all nested
@@ -31,8 +18,8 @@ def f_unpack_dict(dct):
     ----------------
     : unpacked dictionary
     """
-    res = {}
-    for k, v in dct.items():
+    res: dict[str, Any] = {}
+    for (k, v) in dct.items():
         if isinstance(v, dict):
             res = {**res, k: v, **f_unpack_dict(v)}
         else:
@@ -40,10 +27,8 @@ def f_unpack_dict(dct):
     return res
 
 
-def parse_hyperopt_config(config):
-    assert isinstance(config, dict)
-
-    def parse_key(key_dict):
+def parse_hyperopt_config(config: dict[str, Any]) -> dict[str, Any]:
+    def parse_key(key_dict: dict[str, Any]) -> Any:
         if key_dict["type"] == "constant":
             return key_dict["value"]
         elif key_dict["type"] == "uniform":
@@ -61,85 +46,3 @@ def parse_hyperopt_config(config):
     for key, key_dict in config.items():
         parsed_config[key] = parse_key(key_dict)
     return parsed_config
-
-
-def fetch_index_parameters(dbgym_cfg, benchmark_name, data, workload_path):
-    tables = data["protox"]["tables"]
-    attributes = data["protox"]["attributes"]
-    query_spec = data["protox"]["query_spec"]
-
-    workload = Workload(
-        dbgym_cfg, tables, attributes, query_spec, workload_path, pid=None
-    )
-    att_usage = workload.process_column_usage()
-
-    space = IndexSpace(
-        "wolp",
-        tables,
-        max_num_columns=0,
-        index_repr=IndexRepr.ONE_HOT.name,
-        seed=0,
-        latent_dim=0,
-        attributes_overwrite=att_usage,
-    )
-    space._build_mapping(att_usage)
-    max_cat_features = max(
-        len(tables), space.max_num_columns + 1
-    )  # +1 for the one hot encoding.
-    max_attrs = space.max_num_columns + 1  # +1 to account for the table index.
-    return max_attrs, max_cat_features, att_usage, space.class_mapping
-
-
-def load_input_data(dbgym_cfg, input_path, train_size, max_attrs, require_cost, seed):
-    # Load the input data.
-    columns = []
-    columns += ["tbl_index", "idx_class"]
-    columns += [f"col{c}" for c in range(max_attrs - 1)]
-    if require_cost:
-        columns += COST_COLUMNS
-
-    with open_and_save(dbgym_cfg, input_path, mode="rb") as input_file:
-        df = pd.read_parquet(input_file, columns=columns)
-    num_classes = df.idx_class.max() + 1
-
-    # Get the y's and the x's.
-    targets = (COST_COLUMNS + ["idx_class"]) if require_cost else ["idx_class"]
-    y = df[targets].values
-    df.drop(columns=COST_COLUMNS + ["idx_class"], inplace=True, errors="ignore")
-    x = df.values
-    del df
-    gc.collect()
-    gc.collect()
-
-    if train_size == 1:
-        train_dataset = TensorDataset(torch.Tensor(x), torch.Tensor(y))
-        del x
-        gc.collect()
-        gc.collect()
-        return train_dataset, y, y[:, -1], None, num_classes
-
-    # Perform the train test split.
-    train_x, val_x, train_y, val_y = train_test_split(
-        x,
-        y,
-        test_size=1 - train_size,
-        train_size=train_size,
-        random_state=seed,
-        shuffle=True,
-        stratify=y[:, -1],
-    )
-    del x
-    del y
-    gc.collect()
-    gc.collect()
-
-    # Form the tensor datasets.
-    train_dataset = TensorDataset(torch.Tensor(train_x), torch.Tensor(train_y))
-    val_dataset = TensorDataset(torch.Tensor(val_x), torch.Tensor(val_y))
-    del val_x
-    del val_y
-    del train_x
-    gc.collect()
-    gc.collect()
-    logging.info("Train Dataset Size: %s", len(train_dataset))
-    return train_dataset, train_y, train_y[:, -1], val_dataset, num_classes
