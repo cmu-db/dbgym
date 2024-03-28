696831ac57eeb9e7ab2db78da97b3df4238f705d
Patrick Wang <wang.patrick57@gmail.com>
2024-03-28 10:52:10 -0400

    rm exit 0 from pat_test.sh

--- scripts/pat_test.sh
+++ scripts/pat_test.sh
@@ -13,7 +13,6 @@ python3 task.py --no-startup-check dbms postgres pgdata tpch --scale-factor 0.01
 # embedding
 # python3 task.py --no-startup-check tune protox embedding datagen tpch queries_15721_15723 --scale-factor 0.01 --default-sample-limit 64 --file-limit 64 # short datagen for testing
 python3 task.py --no-startup-check tune protox embedding datagen tpch queries_15721_15723 --override-sample-limits "lineitem,32768" --scale-factor 0.01 # long datagen so that train doesn't crash
-exit 0
 python3 task.py --no-startup-check tune protox embedding train tpch queries_15721_15723 --scale-factor 0.01 --iterations-per-epoch 1 --num-samples 4 --train-max-concurrent 4
 
 # agent
