dba13254f9eb9b24e8c4b17a89a28af83976eb84
Patrick Wang <wang.patrick57@gmail.com>
2024-04-02 19:54:08 +0000

    updated pat test and exp scripts

--- experiments/protox_tpch_sf10/main.sh
+++ experiments/protox_tpch_sf10/main.sh
@@ -5,17 +5,17 @@ set -euxo pipefail
 SCALE_FACTOR=10
 
 # benchmark
-python3 task.py --no-startup-check benchmark tpch generate-data $SCALE_FACTOR
-python3 task.py --no-startup-check benchmark tpch generate-workload queries_15721_15723 15721 15723
+python3 task.py --no-startup-check benchmark tpch data $SCALE_FACTOR
+python3 task.py --no-startup-check benchmark tpch workload --scale-factor $SCALE_FACTOR
 
 # postgres
 python3 task.py --no-startup-check dbms postgres build
 python3 task.py --no-startup-check dbms postgres pgdata tpch --scale-factor $SCALE_FACTOR
 
 # embedding
-python3 task.py --no-startup-check tune protox embedding datagen tpch queries_15721_15723 --override-sample-limits "lineitem,32768"
-python3 task.py --no-startup-check tune protox embedding train tpch queries_15721_15723 --scale-factor $SCALE_FACTOR --scale-factor 10 --train-max-concurrent 10
+python3 task.py --no-startup-check tune protox embedding datagen tpch --scale-factor $SCALE_FACTOR --override-sample-limits "lineitem,32768"
+python3 task.py --no-startup-check tune protox embedding train tpch --scale-factor $SCALE_FACTOR --scale-factor 10 --train-max-concurrent 10
 
 # agent
-python3 task.py --no-startup-check tune protox agent hpo tpch queries_15721_15723 --scale-factor $SCALE_FACTOR --max-concurrent 4
-python3 task.py --no-startup-check tune protox agent tune tpch queries_15721_15723 --scale-factor $SCALE_FACTOR
+python3 task.py --no-startup-check tune protox agent hpo tpch --scale-factor $SCALE_FACTOR --max-concurrent 4
+python3 task.py --no-startup-check tune protox agent tune tpch --scale-factor $SCALE_FACTOR
--- scripts/pat_test.sh
+++ scripts/pat_test.sh
@@ -4,17 +4,17 @@ set -euxo pipefail
 
 # benchmark
 python3 task.py --no-startup-check benchmark tpch data 0.01
-python3 task.py --no-startup-check benchmark tpch workload 15721 15721 --scale-factor 0.01
+python3 task.py --no-startup-check benchmark tpch workload --scale-factor 0.01
 
 # postgres
 python3 task.py --no-startup-check dbms postgres build
 python3 task.py --no-startup-check dbms postgres pgdata tpch --scale-factor 0.01
 
 # embedding
-# python3 task.py --no-startup-check tune protox embedding datagen tpch queries_15721_15723 --scale-factor 0.01 --default-sample-limit 64 --file-limit 64 # short datagen for testing
-python3 task.py --no-startup-check tune protox embedding datagen tpch queries_15721_15723 --override-sample-limits "lineitem,32768" --scale-factor 0.01 # long datagen so that train doesn't crash
-python3 task.py --no-startup-check tune protox embedding train tpch queries_15721_15723 --scale-factor 0.01 --iterations-per-epoch 1 --num-points-to-sample 1 --num-batches 1 --batch-size 64 --start-epoch 15 --num-samples 4 --train-max-concurrent 4 --num-curate 2
+# python3 task.py --no-startup-check tune protox embedding datagen tpch --scale-factor 0.01 --default-sample-limit 64 --file-limit 64 # short datagen for testing
+python3 task.py --no-startup-check tune protox embedding datagen tpch --scale-factor 0.01 --override-sample-limits "lineitem,32768" --scale-factor 0.01 # long datagen so that train doesn't crash
+python3 task.py --no-startup-check tune protox embedding train tpch --scale-factor 0.01 --iterations-per-epoch 1 --num-points-to-sample 1 --num-batches 1 --batch-size 64 --start-epoch 15 --num-samples 4 --train-max-concurrent 4 --num-curate 2
 
 # agent
-python3 task.py --no-startup-check tune protox agent hpo tpch queries_15721_15723 --scale-factor 0.01 --num-samples 2 --max-concurrent 2 --duration 0.001
-python3 task.py --no-startup-check tune protox agent tune tpch queries_15721_15723 --scale-factor 0.01
+python3 task.py --no-startup-check tune protox agent hpo tpch --scale-factor 0.01 --num-samples 2 --max-concurrent 2 --duration 0.001
+python3 task.py --no-startup-check tune protox agent tune tpch --scale-factor 0.01
