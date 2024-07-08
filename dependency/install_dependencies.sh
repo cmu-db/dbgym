4ead1a159bb143f35f28372de077cd089fa01bf1
Patrick Wang <wang.patrick57@gmail.com>
2024-07-08 00:38:24 +0000

    made rust installation non-interactive and added . '/home/phw2/.cargo/env'

--- dependency/install_dependencies.sh
+++ dependency/install_dependencies.sh
@@ -2,4 +2,4 @@
 # You may want to create a conda environment before doing this
 pip install -r dependency/requirements.txt
 cat dependency/apt_requirements.txt | xargs sudo apt-get install -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
