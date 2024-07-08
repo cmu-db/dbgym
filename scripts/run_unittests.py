9bfbc8d468503c4301a2c691be905dcc7d808d15
Patrick Wang <wang.patrick57@gmail.com>
2024-07-08 00:27:56 +0000

    now exiting with code 1 if unit tests fail

--- scripts/run_unittests.py
+++ scripts/run_unittests.py
@@ -1,8 +1,12 @@
 import unittest
+import sys
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover(".")
     print(f"suite={suite}")
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+    result = runner.run(suite)
+    if not result.wasSuccessful():
+        # This is needed so that the GHA fails if the unit tests fail.
+        sys.exit(1)
