76b8228489a82aaf52dc8d9ff0178a72c2a87a41
Patrick Wang <wang.patrick57@gmail.com>
2024-08-24 21:48:32 +0000

    simplified format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-find . \( ! -regex '.*/\..*' \) -name '*.py' -exec black {} +
-find . \( ! -regex '.*/\..*' \) -name '*.py' -exec isort --profile black {} +
+black .
+isort .
