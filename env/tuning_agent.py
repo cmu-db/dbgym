e9c70bd37415f46a5a89e3c9e08da2c552d5413d
Patrick Wang <wang.patrick57@gmail.com>
2024-12-20 19:46:41 -0500

    comment in dbmsconfig

--- env/tuning_agent.py
+++ env/tuning_agent.py
@@ -2,15 +2,27 @@ from util.workspace import DBGymConfig
 
 
 class DBMSConfig:
+    """
+    This class can either represent a config or a config delta.
+
+    `indexes` contains a list of SQL statements for creating indexes. If you're using the class
+    as a config delta, it also might contain "DROP ..." statements.
+
+    `sysknobs` contains a mapping from knob names to their values.
+
+    `qknobs` contains a mapping from query IDs to a list of knobs. Each list contains knobs
+    to prepend to the start of the query. The knobs are a list[str] instead of a dict[str, str]
+    because knobs can be settings ("SET (enable_sort on)") or flags ("IndexOnlyScan(it)").
+    """
     def __init__(
         self,
         indexes: list[str],
         sysknobs: dict[str, str],
-        query_knobs: dict[str, list[str]],
+        qknobs: dict[str, list[str]],
     ) -> None:
         self.indexes = indexes
         self.sysknobs = sysknobs
-        self.query_knobs = query_knobs
+        self.qknobs = qknobs
 
 
 class TuningAgent:
