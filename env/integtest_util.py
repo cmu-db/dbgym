9a63b5cc9700c3a7ffc0af58921d0e7e4139ff56
Patrick Wang <wang.patrick57@gmail.com>
2024-12-20 21:41:26 -0500

    made sure INTEGTEST_DBGYM_CFG isn't created more than once

--- env/integtest_util.py
+++ env/integtest_util.py
@@ -22,11 +22,14 @@ class IntegtestWorkspace:
         if not IntegtestWorkspace.get_workspace_path().exists():
             subprocess.run(["./env/set_up_env_integtests.sh"], check=True)
 
-        # The DBGymConfig needs to be created after running ./env/set_up_env_integtests.sh so
-        # that it is created correctly.
-        IntegtestWorkspace.INTEGTEST_DBGYM_CFG = DBGymConfig(
-            IntegtestWorkspace.ENV_INTEGTESTS_DBGYM_CONFIG_FPATH
-        )
+        # Once we get here, we have an invariant that the workspace exists. We need this
+        # invariant to be true in order to create the DBGymConfig.
+        #
+        # However, it also can't be created more than once so we need to check `is None`.
+        if IntegtestWorkspace.INTEGTEST_DBGYM_CFG is None:
+            IntegtestWorkspace.INTEGTEST_DBGYM_CFG = DBGymConfig(
+                IntegtestWorkspace.ENV_INTEGTESTS_DBGYM_CONFIG_FPATH
+            )
 
     @staticmethod
     def get_dbgym_cfg() -> DBGymConfig:
