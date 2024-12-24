a2287d593ac33434ddae6613b0b5f8924bdf4fe6
Patrick Wang <wang.patrick57@gmail.com>
2024-12-23 22:58:56 -0500

    refactored pgconn to use metadata

--- env/integtest_pg_conn.py
+++ env/integtest_pg_conn.py
@@ -3,22 +3,12 @@ import unittest
 
 import psycopg
 
-from env.integtest_util import (
-    INTEGTEST_BENCHMARK,
-    INTEGTEST_SCALE_FACTOR,
-    IntegtestWorkspace,
-)
+from env.integtest_util import IntegtestWorkspace, MockTuningAgent
 from env.pg_conn import PostgresConn
 from util.pg import (
     DEFAULT_POSTGRES_PORT,
     get_is_postgres_running,
     get_running_postgres_ports,
-)
-from util.workspace import (
-    DEFAULT_BOOT_CONFIG_FPATH,
-    get_default_dbdata_parent_dpath,
-    get_default_pgbin_path,
-    get_default_pristine_dbdata_snapshot_path,
 )
 
 
@@ -33,18 +23,7 @@ class PostgresConnTests(unittest.TestCase):
             "Make sure Postgres isn't running before starting the integration test. `pkill postgres` is one way "
             + "to ensure this. Be careful about accidentally taking down other people's Postgres instances though.",
         )
-        # TODO: refactor this using metadata
-        self.pristine_dbdata_snapshot_path = get_default_pristine_dbdata_snapshot_path(
-            IntegtestWorkspace.get_workspace_path(),
-            INTEGTEST_BENCHMARK,
-            INTEGTEST_SCALE_FACTOR,
-        )
-        self.dbdata_parent_dpath = get_default_dbdata_parent_dpath(
-            IntegtestWorkspace.get_workspace_path()
-        )
-        self.pgbin_dpath = get_default_pgbin_path(
-            IntegtestWorkspace.get_workspace_path()
-        )
+        self.metadata = MockTuningAgent.get_default_metadata()
 
         # The reason we restart Postgres every time is to ensure a "clean" starting point
         # so that all tests are independent of each other.
@@ -61,9 +40,9 @@ class PostgresConnTests(unittest.TestCase):
         return PostgresConn(
             IntegtestWorkspace.get_dbgym_cfg(),
             pgport,
-            self.pristine_dbdata_snapshot_path,
-            self.dbdata_parent_dpath,
-            self.pgbin_dpath,
+            self.metadata.pristine_dbdata_snapshot_path,
+            self.metadata.dbdata_parent_path,
+            self.metadata.pgbin_path,
             None,
         )
 
