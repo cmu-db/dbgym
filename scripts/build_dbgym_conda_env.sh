8ebbc24d7bfbf7109bd531dbc830c457e9cd39f2
Patrick Wang <wang.patrick57@gmail.com>
2024-12-25 17:56:16 -0500

    changed install dep step of ci

--- scripts/build_dbgym_conda_env.sh
+++ scripts/build_dbgym_conda_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script builds the conda environment used by the gym itself (i.e. the orchestrator).
+# This script is optional. You don't need to use conda if you don't want to (the CI doesn't use conda, for instance)
 
 set -euo pipefail
 
