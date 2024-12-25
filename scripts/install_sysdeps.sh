95f0683443ff6b8610f5e3d5d2e682117aa53b0c
Patrick Wang <wang.patrick57@gmail.com>
2024-12-25 17:43:02 -0500

    comment

--- scripts/install_sysdeps.sh
+++ scripts/install_sysdeps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# "sysdeps" stands for "system dependencies".
 # These are dependencies unrelated to Python that the dbgym needs.
 cat scripts/configs/apt_requirements.txt | xargs sudo apt-get install -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
