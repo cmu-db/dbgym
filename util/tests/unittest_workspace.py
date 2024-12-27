3f956329a7cbbbe934300ee369c4f656dbc40a07
Patrick Wang <wang.patrick57@gmail.com>
2024-12-27 16:37:30 -0500

    refactored workspace tests to use helpers

--- util/tests/unittest_workspace.py
+++ util/tests/unittest_workspace.py
@@ -26,104 +26,88 @@ class WorkspaceTests(unittest.TestCase):
         if self.scratchspace_path.exists():
             shutil.rmtree(self.scratchspace_path)
 
-        # Reset this to avoid the error of it being created twice.
-        # The assertion checking that it's only been been created once is meant to avoid creating multiple
-        # objects for the same workspace. However, it's fine to create it multiple times in this case because
-        # we're deleting the scratchspace before each test.
-        DBGymWorkspace.num_times_created_this_run = 0
+        self.workspace = None
+        self.expected_structure = None
 
     def tearDown(self) -> None:
         # You can comment this out if you want to inspect the scratchspace after a test (often used for debugging).
         if self.scratchspace_path.exists():
             shutil.rmtree(self.scratchspace_path)
 
-    @staticmethod
-    def get_workspace_init_structure(workspace: DBGymWorkspace) -> FilesystemStructure:
-        symlinks_structure = FilesystemStructure({})
-        task_runs_structure = FilesystemStructure(
-            {
-                "latest_run.link": (
-                    "symlink",
-                    f"dbgym_workspace/task_runs/{workspace.dbgym_this_run_path.name}",
-                ),
-                workspace.dbgym_this_run_path.name: {},
-            }
-        )
-        return make_workspace_structure(symlinks_structure, task_runs_structure)
+    # All these helper functions will perform an action, update the expected structure, and then verify the structure.
+    def init_workspace_helper(self) -> None:
+        # Reset this to avoid the error of it being created twice.
+        # In real usage, the second run would be a different Python process so DBGymWorkspace.num_times_created_this_run would be 0.
+        DBGymWorkspace.num_times_created_this_run = 0
+        self.workspace = DBGymWorkspace(self.workspace_path)
 
-    @staticmethod
-    def get_updated_structure_from_workspace_init(
-        structure: FilesystemStructure, workspace: DBGymWorkspace
-    ) -> FilesystemStructure:
-        structure["dbgym_workspace"]["task_runs"][
-            workspace.dbgym_this_run_path.name
-        ] = {}
-        structure["dbgym_workspace"]["task_runs"]["latest_run.link"] = (
-            "symlink",
-            f"dbgym_workspace/task_runs/{workspace.dbgym_this_run_path.name}",
+        if self.expected_structure is None:
+            self.expected_structure = make_workspace_structure(
+                FilesystemStructure({}),
+                FilesystemStructure(
+                    {
+                        "latest_run.link": (
+                            "symlink",
+                            f"dbgym_workspace/task_runs/{self.workspace.dbgym_this_run_path.name}",
+                        ),
+                        self.workspace.dbgym_this_run_path.name: {},
+                    }
+                ),
+            )
+        else:
+            self.expected_structure["dbgym_workspace"]["task_runs"][
+                self.workspace.dbgym_this_run_path.name
+            ] = {}
+            self.expected_structure["dbgym_workspace"]["task_runs"]["latest_run.link"] = (
+                "symlink",
+                f"dbgym_workspace/task_runs/{self.workspace.dbgym_this_run_path.name}",
+            )
+
+        self.assertTrue(
+            verify_structure(self.scratchspace_path, self.expected_structure)
         )
-        return structure
+
+    def make_result_helper(self, result_name: str="result.txt") -> Path:
+        result_path = self.workspace.dbgym_this_run_path / result_name
+        result_path.touch()
+        self.expected_structure["dbgym_workspace"]["task_runs"][
+            self.workspace.dbgym_this_run_path.name
+        ][result_name] = ("file",)
+        self.assertTrue(verify_structure(self.scratchspace_path, self.expected_structure))
+        return result_path
+
+    def link_result_helper(self, result_path: Path) -> None:
+        self.workspace.link_result(result_path)
+        self.expected_structure["dbgym_workspace"]["symlinks"]["dbgym"] = {}
+        self.expected_structure["dbgym_workspace"]["symlinks"]["dbgym"][
+            f"{result_path.name}.link"
+        ] = (
+            "symlink",
+            f"dbgym_workspace/task_runs/{self.workspace.dbgym_this_run_path.name}/{result_path.name}",
+        )
+        self.assertTrue(verify_structure(self.scratchspace_path, self.expected_structure))
 
     def test_init_fields(self) -> None:
         workspace = DBGymWorkspace(self.workspace_path)
         self.assertEqual(workspace.app_name, "dbgym")
 
     def test_init_from_nonexistent_workspace(self) -> None:
-        starting_structure = FilesystemStructure({})
-        create_structure(self.scratchspace_path, starting_structure)
-
-        workspace = DBGymWorkspace(self.workspace_path)
-        ending_structure = WorkspaceTests.get_workspace_init_structure(workspace)
-
-        self.assertTrue(verify_structure(self.scratchspace_path, ending_structure))
+        self.init_workspace_helper()
 
     def test_init_from_empty_workspace(self) -> None:
         starting_structure = FilesystemStructure({"dbgym_workspace": {}})
         create_structure(self.scratchspace_path, starting_structure)
 
-        workspace = DBGymWorkspace(self.workspace_path)
-        ending_structure = WorkspaceTests.get_workspace_init_structure(workspace)
-
-        self.assertTrue(verify_structure(self.scratchspace_path, ending_structure))
+        self.init_workspace_helper()
 
     def test_init_from_already_initialized_workspace(self) -> None:
-        # This first initialization will create a task run.
-        workspace = DBGymWorkspace(self.workspace_path)
-        ending_structure = WorkspaceTests.get_workspace_init_structure(workspace)
-
-        # The second initialization will create a second task run.
-        # Make sure to reset this. In real usage, the second run would be a different Python process
-        # so DBGymWorkspace.num_times_created_this_run would be 0.
-        DBGymWorkspace.num_times_created_this_run = 0
-        workspace = DBGymWorkspace(self.workspace_path)
-        ending_structure = WorkspaceTests.get_updated_structure_from_workspace_init(
-            ending_structure, workspace
-        )
-
-        self.assertTrue(verify_structure(self.scratchspace_path, ending_structure))
+        self.init_workspace_helper()
+        self.init_workspace_helper()
 
     def test_link_result_basic_functionality(self) -> None:
-        # Setup.
-        workspace = DBGymWorkspace(self.workspace_path)
-        ending_structure = WorkspaceTests.get_workspace_init_structure(workspace)
-
-        # Make a result file.
-        result_path = workspace.dbgym_this_run_path / "result.txt"
-        result_path.touch()
-        ending_structure["dbgym_workspace"]["task_runs"][
-            workspace.dbgym_this_run_path.name
-        ]["result.txt"] = ("file",)
-
-        # Link the result file.
-        workspace.link_result(result_path)
-        ending_structure["dbgym_workspace"]["symlinks"]["dbgym"] = {}
-        ending_structure["dbgym_workspace"]["symlinks"]["dbgym"]["result.txt.link"] = (
-            "symlink",
-            f"dbgym_workspace/task_runs/{workspace.dbgym_this_run_path.name}/result.txt",
-        )
-
-        # Verify structure.
-        self.assertTrue(verify_structure(self.scratchspace_path, ending_structure))
+        self.init_workspace_helper()
+        result_path = self.make_result_helper()
+        self.link_result_helper(result_path)
 
     # TODO: test overriding existing symlink
     # TODO: test linking result from another run should raise
