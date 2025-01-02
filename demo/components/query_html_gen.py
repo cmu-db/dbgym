61fda506f0ba6d722e047f2983485f3742ef8f29
Patrick Wang <wang.patrick57@gmail.com>
2025-01-01 18:05:30 -0800

    query 11a

--- demo/components/query_html_gen.py
+++ demo/components/query_html_gen.py
@@ -1,6 +1,8 @@
 def query_to_html(query: str) -> str:
+    assert "='" not in query, "Some queries in the repo are incorrectly formatted this way"
+
     # Keywords
-    keywords = ["SELECT", "FROM", "WHERE", "AND", "OR", "AS", "LIKE"]
+    keywords = ["SELECT", "FROM", "WHERE", "AND", "OR", "AS", "LIKE", "!=", " = ", "NULL"]
     for keyword in keywords:
         query = query.replace(keyword, f"<span class='query-red'>{keyword}</span>")
 
@@ -10,25 +12,47 @@ def query_to_html(query: str) -> str:
         query = query.replace(function, f"<span class='query-lightblue'>{function}</span>")
 
     # Tables
-    tables = ["ct", "it", "mc", "mi_idx", "t"]
+    tables = ["ct", "it", "lt", "t", "mc", "mi_idx", "cn", "mk", "k", "ml"]
     for table in tables:
         query = query.replace(f"{table}.", f"<span class='query-lightblue'>{table}</span>.")
 
     # Columns
-    columns = ["note", "title", "production_year", "id", "movie_id", "company_type_id", "info_type_id", "kind", "info"]
+    columns = [
+        "note", "title", "production_year", "id", "movie_id", "company_type_id", "info_type_id", "kind", "info",
+        "keyword_id", "link_type_id", "company_id", "country_code", "name", "keyword", "link"
+    ]
     for column in columns:
         query = query.replace(f".{column}", f".<span class='query-lightblue'>{column}</span>")
 
     # Strings
-    query = query.replace(" '", " <span class='query-darkblue'>'")
-    query = query.replace("' ", "'</span> ")
-    query = query.replace("')", "'</span>)")
+    strings = [
+        "production companies",
+        "top 250 rank",
+        "%(as Metro-Goldwyn-Mayer Pictures)%",
+        "%(co-production)%",
+        "%(presents)%",
+        "[pl]",
+        "%Film%",
+        "%Warner%",
+        "sequel",
+        "%follow%",
+    ]
+    for string in strings:
+        query = query.replace(f"'{string}'", f"<span class='query-darkblue'>'{string}'</span>")
+
+    # Numbers
+    numbers = [
+        "1950",
+        "2000"
+    ]
+    for number in numbers:
+        query = query.replace(f"{number}", f"<span class='query-lightblue'>{number}</span>")
 
     return query
 
 
 if __name__ == "__main__":
-    query = """SELECT MIN(mc.note) AS production_note,
+    query1a = """SELECT MIN(mc.note) AS production_note,
        MIN(t.title) AS movie_title,
        MIN(t.production_year) AS movie_year<br>
 FROM company_type AS ct,
@@ -46,5 +70,36 @@ WHERE ct.kind = 'production companies'
   AND t.id = mi_idx.movie_id
   AND mc.movie_id = mi_idx.movie_id
   AND it.id = mi_idx.info_type_id;"""
-    html_query = query_to_html(query)
+    
+    query11a = """SELECT MIN(cn.name) AS from_company,
+       MIN(lt.link) AS movie_link_type,
+       MIN(t.title) AS non_polish_sequel_movie<br>
+FROM company_name AS cn,
+     company_type AS ct,
+     keyword AS k,
+     link_type AS lt,
+     movie_companies AS mc,
+     movie_keyword AS mk,
+     movie_link AS ml,
+     title AS t<br>
+WHERE cn.country_code != '[pl]'
+  AND (cn.name LIKE '%Film%'
+       OR cn.name LIKE '%Warner%')
+  AND ct.kind = 'production companies'
+  AND k.keyword = 'sequel'
+  AND lt.link LIKE '%follow%'
+  AND mc.note IS NULL
+  AND t.production_year BETWEEN 1950 AND 2000
+  AND lt.id = ml.link_type_id
+  AND ml.movie_id = t.id
+  AND t.id = mk.movie_id
+  AND mk.keyword_id = k.id
+  AND t.id = mc.movie_id
+  AND mc.company_type_id = ct.id
+  AND mc.company_id = cn.id
+  AND ml.movie_id = mk.movie_id
+  AND ml.movie_id = mc.movie_id
+  AND mk.movie_id = mc.movie_id;"""
+
+    html_query = query_to_html(query1a)
     print(html_query)
