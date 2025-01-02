d0ca826f116d14592ae7843c4a5e30e8a2573f78
Patrick Wang <wang.patrick57@gmail.com>
2025-01-01 19:06:22 -0800

    fixed how queries are loaded

--- demo/js/loadPopupHtml.js
+++ demo/js/loadPopupHtml.js
@@ -1,20 +1,24 @@
-fetch('/components/genned_query1a.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('query1View').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading HTML:', error));
+function initializePopup() {
+    fetch('/components/genned_query1a.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('query1View').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
 
-fetch('/components/genned_query3b.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('query2View').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading HTML:', error));
+    fetch('/components/genned_query3b.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('query2View').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
 
-fetch('/components/genned_query6a.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('query3View').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading HTML:', error));
+    fetch('/components/genned_query6a.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('query3View').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+initializePopup();
