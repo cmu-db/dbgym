9ba52c5e8b1613f56bc0b85d09cdf74767ee8667
Patrick Wang <wang.patrick57@gmail.com>
2025-01-03 17:29:16 -0800

    made backend a class

--- demo/backend/backend.py
+++ demo/backend/backend.py
@@ -10,34 +10,45 @@ from gymlib.pg_conn import PostgresConn
 from gymlib.workload import Workload
 from gymlib.workspace import fully_resolve_path, make_standard_dbgym_workspace
 
+class Backend:
+    def __init__(self):
+        self.dbgym_workspace = make_standard_dbgym_workspace()
+        self.pg_conn = PostgresConn(
+            self.dbgym_workspace,
+            DEFAULT_POSTGRES_PORT,
+            fully_resolve_path(
+                get_dbdata_tgz_symlink_path(
+                    self.dbgym_workspace.dbgym_workspace_path, "job", DEFAULT_SCALE_FACTOR
+                )
+            ),
+            self.dbgym_workspace.dbgym_tmp_path,
+            fully_resolve_path(
+                get_pgbin_symlink_path(self.dbgym_workspace.dbgym_workspace_path)
+            ),
+            None,
+        )
+        self.workload = Workload(
+            self.dbgym_workspace,
+            fully_resolve_path(
+                get_workload_symlink_path(
+                    self.dbgym_workspace.dbgym_workspace_path,
+                    "job",
+                    DEFAULT_SCALE_FACTOR,
+                    get_workload_suffix("job", query_subset="demo"),
+                )
+            ),
+        )
+        self.pg_conn.restore_pristine_snapshot()
+        self.pg_conn.restart_postgres()
+
+    def time_workload(self):
+        return self.pg_conn.time_workload(self.workload)
+
+    def shutdown_postgres(self):
+        self.pg_conn.shutdown_postgres()
+
+
 if __name__ == "__main__":
-    dbgym_workspace = make_standard_dbgym_workspace()
-    pg_conn = PostgresConn(
-        dbgym_workspace,
-        DEFAULT_POSTGRES_PORT,
-        fully_resolve_path(
-            get_dbdata_tgz_symlink_path(
-                dbgym_workspace.dbgym_workspace_path, "job", DEFAULT_SCALE_FACTOR
-            )
-        ),
-        dbgym_workspace.dbgym_tmp_path,
-        fully_resolve_path(
-            get_pgbin_symlink_path(dbgym_workspace.dbgym_workspace_path)
-        ),
-        None,
-    )
-    workload = Workload(
-        dbgym_workspace,
-        fully_resolve_path(
-            get_workload_symlink_path(
-                dbgym_workspace.dbgym_workspace_path,
-                "job",
-                DEFAULT_SCALE_FACTOR,
-                get_workload_suffix("job", query_subset="demo"),
-            )
-        ),
-    )
-    pg_conn.restore_pristine_snapshot()
-    pg_conn.restart_postgres()
-    print(pg_conn.time_workload(workload))
-    pg_conn.shutdown_postgres()
+    backend = Backend()
+    backend.time_workload()
+    backend.shutdown_postgres()
